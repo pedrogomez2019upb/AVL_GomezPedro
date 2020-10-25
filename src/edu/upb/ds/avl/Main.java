ec4903c708b0d9cb42ae72fa705e4f44bdad4811
Pedro Felipe Gómez Bonilla <pedro.gomez.2019@upb.edu.co>
2020-10-25 17:41:29 -0500

    Modification

--- src/edu/upb/ds/avl/Main.java
+++ src/edu/upb/ds/avl/Main.java
@@ -7,12 +7,28 @@ public class Main {
         System.out.println("\nBienvenido al arbol AVL");
         System.out.println("\n############################");
         AVLTree tree = new AVLTree();
+
+        tree.root = tree.insertar(tree.root, 60);
+        tree.root = tree.insertar(tree.root, 57);
+        tree.root = tree.insertar(tree.root, 76);
+        tree.root = tree.insertar(tree.root, 48);
+        tree.root = tree.insertar(tree.root, 59);
+        tree.root = tree.insertar(tree.root, 33);
+        tree.root = tree.insertar(tree.root, 55);
+        tree.root = tree.insertar(tree.root, 65);
+        tree.root = tree.insertar(tree.root, 83);
+        tree.root = tree.insertar(tree.root, 63);
+
+
+        /*
         tree.root = tree.insertar(tree.root, 10);
         tree.root = tree.insertar(tree.root, 20);
         tree.root = tree.insertar(tree.root, 30);
         tree.root = tree.insertar(tree.root, 40);
         tree.root = tree.insertar(tree.root, 50);
         tree.root = tree.insertar(tree.root, 25);
+
+         */
         System.out.println("\n############################");
         System.out.println("\nAVL en Orden Transversal : ");
         System.out.println("\n############################");
